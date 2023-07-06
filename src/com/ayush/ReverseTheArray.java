5985ea804defbaa4a68719d0ede75f09909ce35e
ayushjadhavvv <91875936+ayushjadhavvv@users.noreply.github.com>
2023-07-06 11:35:20 +0530

    Write a program to reverse an array or string
    Input  : arr[] = {1,3,23,9,18,56}
    Output : arr[] = {56,18,9,23,3,1}

--- src/com/ayush/ReverseTheArray.java
+++ src/com/ayush/ReverseTheArray.java
@@ -1,38 +1,26 @@
-package com.ayush;
+import java.util.Arrays;
 
-import java.sql.SQLOutput;
-import java.util.*;
-
-public class ReverseTheArray {
-
+public class Swap {
     public static void main(String[] args) {
-        //Take inputs from user
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array");
-
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-
-        //take in values of array
-        for(int i = 0; i<n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        //main part of the code
-        int temp;
+        int[] arr = {1,3,23,9,18,56};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    static void reverse(int[] arr) {
         int start = 0;
-        int end = n-1;
+        int end = arr.length-1;
+        
         while (start < end) {
-            temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
+            swap(arr, start, end);
             start++;
             end--;
         }
-        System.out.println("The values after reversal are");
-        for(int i = 0; i<n; i++) {
-            System.out.println(arr[i]);
-        }
+    }
+    
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
